b226063db88900987f57e2b19f4989549b55341f
zx07252000 <zx07252000@naver.com>
2018-09-17 22:55:10 +0900

    첫번째 이동후 두번째 이동
    첫번째 이동후 두번째 이동
    기울기가 달라 속도를 맞추어주기 위해
    속도 조절

--- Drills/Drill-05/Driil_05.py
+++ Drills/Drill-05/Driil_05.py
@@ -9,7 +9,7 @@ x=0
 y=0
 frame=0
 
-def move_point_to_point(x1,y1,x2,y2,frame):
+def move_point_to_point1(x1,y1,x2,y2,frame):
 
     if(x1>x2):
         x_distance=x1-x2
@@ -27,15 +27,40 @@ def move_point_to_point(x1,y1,x2,y2,frame):
         character.clip_draw(frame*100,0,100,100,x1,y1)
         update_canvas()
         frame=(frame+1)%8
-        x1-=2
-        y1-=2*gradient
+        x1-=1
+        y1-=1*gradient
 
-        delay(0.05)
+        delay(0.01)
+        get_events()
+def move_point_to_point2(x1,y1,x2,y2,frame):
+
+    if(x1>x2):
+        x_distance=x1-x2
+        y_distance=y1-y2
+        gradient=y_distance//x_distance
+
+    if(x2>x1):
+        x_distance = x2 - x1
+        y_distance = y2 - y1
+        gradient=y_distance//x_distance
+
+    while(x1<x2):
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frame*100,100,100,100,x1,y1)
+        update_canvas()
+        frame=(frame+1)%8
+        x1+=3
+        y1+=3*gradient
+
+        delay(0.01)
         get_events()
 
-
 while True:
-    move_point_to_point(203,535,132,243,frame)
+    move_point_to_point1(203,535,132,243,frame)
+    move_point_to_point2(132, 243, 535, 470, frame)
+
+
 
 
 
