ddbce99cb8ec2ffb6c1b8a80d5e74df26374f794
zx07252000 <zx07252000@naver.com>
2018-10-04 00:08:05 +0900

    타이틀 화면 출력
    2015180036 장영진 커밋횟수 7회
    타이틀 화면 출력
    배경화면 출력 // 화면 크기조절및/
    캔버스 안나오게 수정 필요

---
+++ 1차 발표/Back_ground.py
@@ -0,0 +1,26 @@
+from pico2d import *
+
+
+
+Start_background_WIDTH,Start_background_HEIGHT=600,600
+
+open_canvas(Start_background_WIDTH,Start_background_HEIGHT)
+
+Start_background = load_image('Title background.png')
+
+x=0
+y=0
+frame=0
+Background=True
+
+while Background:
+    clear_canvas()
+    Start_background.draw(Start_background_WIDTH//2+50,Start_background_HEIGHT//2-220)
+    update_canvas()
+    frame=(frame+1)%1
+    delay(0.01)
+    get_events()
+
+
+close_canvas()
+
