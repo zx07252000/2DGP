6490f8f31801d80120a992ab4e928c72e3a91db6
zx07252000 <zx07252000@naver.com>
2018-10-07 14:41:36 +0900

    Back ground 수정
    Back ground 수정 및 첫화면 제작

--- 1차 발표/Back_ground.py
+++ 1차 발표/Back_ground.py
@@ -2,14 +2,25 @@ from pico2d import *
 
 
 
-Start_background_WIDTH,Start_background_HEIGHT=600,600
+Start_background_WIDTH,Start_background_HEIGHT=1020,897
 
 open_canvas(Start_background_WIDTH,Start_background_HEIGHT)
 
-Start_background = load_image('Title background.png')
+Start_background = load_image('Title background2.png')
 
 def handle_events():
-    pass
+    global moving
+    global x,y
+    events = get_events()
+
+    for event in events:
+        if event.type==SDL_QUIT:
+            moving = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, Start_background_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            moving = False
+
 
 
 def Start_background_to_Character_background():
@@ -22,19 +33,19 @@ def Character_background_to_GamePlay_background():
 
 
 
-
-
 x=0
 y=0
 frame=0
 Background=True
 
 while Background:
+
     clear_canvas()
-    Start_background.draw(Start_background_WIDTH//2+50,Start_background_HEIGHT//2-220)
+    Start_background.draw(Start_background_WIDTH//2,Start_background_HEIGHT//2)
     update_canvas()
     frame=(frame+1)%1
     delay(0.01)
+    handle_events()
     get_events()
 
 
