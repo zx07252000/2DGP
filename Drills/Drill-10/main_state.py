5ae12633dcdc9baf101e1efb96a68e33126be8b9
zx07252000 <zx07252000@naver.com>
2018-10-11 19:20:10 +0900

    Main-state 함수 조절
    2015180036 장영진 커밋횟수 2회
    Drill-10
    Main_state 함수에서 정지 화면까지 연결 함수 push를써서 연결해줌

--- Drills/Drill-10/main_state.py
+++ Drills/Drill-10/main_state.py
@@ -73,7 +73,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_q:
+            game_framework.push_state(pause1)
 
 
     pass
