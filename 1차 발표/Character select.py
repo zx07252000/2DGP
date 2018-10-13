6058829d602f1cc40598565052f57240704dc2e7
zx07252000 <zx07252000@naver.com>
2018-10-13 22:49:17 +0900

    Character select 함수 추가
    2015180036 장영진 커밋횟수 18회
    Character select 함수 추가및변경

--- 1차 발표/Character select.py
+++ 1차 발표/Character select.py
@@ -8,13 +8,17 @@ import game_framework
 import Main_Screen
 
 name ="Character select"
-
+image = None
 
 def enter():
+    global image
+    image = load_image('character select.png')
     pass
 
 
 def exit():
+    global image
+    del (image)
     pass
 
 
@@ -23,10 +27,21 @@ def pause():
 
 
 def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.change_state(Main_Screen)
+           
     pass
 
 
 def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
     pass
 
 
