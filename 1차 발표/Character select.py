23f25aa7e888d26f7b13b06f026577cb2b64346d
zx07252000 <zx07252000@naver.com>
2018-10-13 22:46:41 +0900

    Character select Screen 함수작성
    2015180036 장영진 커밋횟수 17회
    Character select Screen 함수작성

---
+++ 1차 발표/Character select.py
@@ -0,0 +1,38 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import Main_Screen
+
+name ="Character select"
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def pause():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def draw():
+    pass
+
+
+def update():
+    pass
+
+
+def resume():
+    pass
