b6ed5f0d19527ec584afacc6441d4d248ed924dd
zx07252000 <zx07252000@naver.com>
2018-11-03 04:04:57 +0900

    메인화면 리소스 및 애니메이션 수정
    2015180036 장영진 커밋횟수 58회
    메인화면 리소스 및 애니메이션 수정

--- 1차 발표/Main_Screen.py
+++ 1차 발표/Main_Screen.py
@@ -5,19 +5,39 @@ import Character_select
 
 name="Main_Screen"
 image=None
+new=None
+option=None
+option_L=None
+Exit=None
+Exit_L=None
+change=4
 
 
 def enter():
-    global image
+    global image,new,option,option_L,Exit,Exit_L
+
     image=load_image('Resource\\Main_Screen.png')
+    new=load_image('Resource\\newgame.png')
+    option=load_image('Resource\\option.png')
+    option_L=load_image('Resource\\Option_L.png')
+    Exit=load_image('Resource\\Exit.png')
+    Exit_L = load_image('Resource\\Exit_L.png')
+
     pass
 
 def exit():
-    global image
+    global image,new,option,option_L,Exit,Exit_L
+    del(new)
     del(image)
+    del(option)
+    del(option_L)
+    del(Exit)
+    del (Exit_L)
+
     pass
 
 def handle_events():
+    global change
     events=get_events()
     for event in events:
         if event.type==SDL_QUIT:
@@ -25,14 +45,39 @@ def handle_events():
         else:
             if(event.type,event.key)==(SDL_KEYDOWN,SDLK_ESCAPE):
                 game_framework.quit()
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
+                change=change-1
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
+                change=change+1
+            elif change==4and(event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
                 game_framework.change_state(Character_select)
+
 
     pass
 
 def draw():
+
     clear_canvas()
-    image.draw(512,382)
+    image.clip_draw(0, 0, 1020, 767, 512, 382)
+    if(change==4):
+        new.clip_draw(0, 30, 200, 45, 500, 382)
+        option_L.clip_draw(0, 0, 150, 30, 510, 250)
+        Exit_L.clip_draw(0, 0, 150, 40, 510, 182)
+    if(change==3):
+        new.clip_draw(0, 0, 200, 25, 500, 382)
+        option_L.clip_draw(0, 0, 150, 30, 510, 250)
+        Exit_L.clip_draw(0, 0, 150, 40, 510, 182)
+    if(change==2):
+        new.clip_draw(0, 0, 200, 25, 500, 382)
+        option.clip_draw(0, 0, 200, 30, 510, 250)
+        Exit_L.clip_draw(0, 0, 150, 40, 510, 182)
+    if (change == 1):
+        new.clip_draw(0, 0, 200, 25, 500, 382)
+        option_L.clip_draw(0, 0, 150, 30, 510, 250)
+        Exit.clip_draw(0,0, 200, 50, 510, 182)
+    print(change)
+
+
     update_canvas()
     pass
 
